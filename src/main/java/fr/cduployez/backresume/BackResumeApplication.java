f4f09271ad582753b760855e61a3257b853d7d8d
cduployez <cduployez+githubpro@gmail.com>
2023-07-08 11:17:56 +0200

    Initial commit

---
+++ src/main/java/fr/cduployez/backresume/BackResumeApplication.java
@@ -0,0 +1,13 @@
+package fr.cduployez.backresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackResumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cduployez/backresume/BackResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cduployez.backresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
